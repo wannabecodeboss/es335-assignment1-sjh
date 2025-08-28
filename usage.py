dd29f12e6b891e3c257850ca7d8dcb5c3265edf9
Hiteshi Meisheri <152746571+wannabecodeboss@users.noreply.github.com>
2025-08-28 18:23:58 +0530

    Update usage.py

--- usage.py
+++ usage.py
@@ -68,8 +68,8 @@ for criteria in ["information_gain"]:
     print("Criteria :", criteria)
     print("Accuracy: ", accuracy(y_hat, y))
     for cls in y.unique():
-        print("Precision: ", precision(y_hat, y, cls))
-        print("Recall: ", recall(y_hat, y, cls))
+        print("Precision: ", precision(y_hat, y, cls)) # (no. of true positive predictions) / (true positive + false positive predictions)
+        print("Recall: ", recall(y_hat, y, cls)) # (no. of true positive predictions) / (true positive + false negative predictions)
 
 # Test case 4
 # Discrete Input and Real Output
@@ -79,7 +79,7 @@ P = 5
 X = pd.DataFrame({i: pd.Series(np.random.randint(P, size=N), dtype="category") for i in range(5)})
 y = pd.Series(np.random.randn(N))
 
-for criteria in ["information_gain", "gini_index"]:
+for criteria in ["information_gain"]:
     tree = DecisionTree(criterion=criteria)  # Split based on Inf. Gain
     tree.fit(X, y)
     y_hat = tree.predict(X)
@@ -87,3 +87,5 @@ for criteria in ["information_gain", "gini_index"]:
     print("Criteria :", criteria)
     print("RMSE: ", rmse(y_hat, y))
     print("MAE: ", mae(y_hat, y))
+
+
